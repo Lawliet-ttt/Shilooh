aada263ab9e7f6a0e3921cc7a950fd852debe061
Lawliet-ttt <higashizumu08@hotmail.com>
2024-12-02 15:46:56 -0500

    Prueba

---
+++ lib/PantallaInicio.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+ class PantallaInicio extends StatefulWidget {
+  const PantallaInicio({super.key});
+
+  @override
+  State<PantallaInicio> createState() => _PantallaInicio();
+}
+
+class _PantallaInicio extends State<PantallaInicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        
+      ),
+    );
+  }
+}
